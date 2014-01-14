b3b3414ff5c164b0a32105ee621e53c122071f94
Petar Petrov <codemastering@gmail.com>
2014-01-14 10:32:55 +0200

    Change - Remove unvaluable test.

--- Blast.Core.Tests/Search/SearchOptionTests.cs
+++ Blast.Core.Tests/Search/SearchOptionTests.cs
@@ -32,32 +32,6 @@ namespace Blast.Core.Tests.Search
 			Assert.AreEqual(name, o.Name);
 			Assert.AreSame(f, o.IsMatch);
 		}
-
-		[Test]
-		public void Count_SetValue_FiresEvent()
-		{
-			var value = 23;
-			var name = @"A";
-			Func<ViewModel, bool> f = v => true;
-
-			var fired = false;
-			var o = Substitute.For<SearchOption<ViewModel>>(name, f, false);
-			PropertyChangedEventHandler handler = (sender, args) => { fired = true; };
-			try
-			{
-				o.PropertyChanged += handler;
-				o.Count = value;
-			}
-			finally
-			{
-				o.PropertyChanged -= handler;
-			}
-
-			Assert.AreEqual(name, o.Name);
-			Assert.AreSame(f, o.IsMatch);
-			Assert.AreEqual(value, o.Count);
-			Assert.IsTrue(fired);
-		}
 	}
 
 	public class TestSearchOption : SearchOption<TestViewModel>
